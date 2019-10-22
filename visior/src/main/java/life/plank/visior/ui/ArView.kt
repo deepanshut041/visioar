24402240dfe8f15b1c41eb001d2dfe12864612e5
deepanshut041 <deepanshut041@gmail.com>
2019-10-22 14:46:55 +0530

    Added ar core support

--- visior/src/main/java/life/plank/visior/ui/ArView.kt
+++ visior/src/main/java/life/plank/visior/ui/ArView.kt
@@ -2,7 +2,9 @@ package life.plank.visior.ui
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Point
 import android.hardware.camera2.CameraManager
+import android.net.Uri
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -17,6 +19,14 @@ import org.koin.core.inject
 import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import life.plank.visior.data.view.ArPointData
 import timber.log.Timber
 
@@ -29,7 +39,7 @@ class ArView @JvmOverloads constructor(
         View.inflate(context, R.layout.ar_layout, this)
     }
 
-
+    private lateinit var arFragment: ArFragment
     private var dependencyProvider: DependencyProvider? = null
     var isArViewStarted: Boolean = false
 
@@ -69,22 +79,31 @@ class ArView @JvmOverloads constructor(
                     else -> Toast.makeText(
                         dependencyProvider!!.getContext(),
                         "Please Allow Permissions",
-                        Toast.LENGTH_SHORT).show()
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
     private fun startCamera() {
-        arCameraView.setCameraManager(cameraManager)
-        arCameraView.setSceneForm(dependencyProvider!!.getPermissionActivity())
-        arCameraView.onStart()
+//        arCameraView.setCameraManager(cameraManager)
+//        arCameraView.setSceneForm(dependencyProvider!!.getPermissionActivity())
+//        arCameraView.onStart()
     }
 
     @SuppressLint("LogNotTimber")
     private fun setSensorDataListeners() {
         dependencyProvider?.let {
             arMapView.onCreate(it)
+            arFragment =
+                (it.getPermissionActivity() as AppCompatActivity).supportFragmentManager.findFragmentById(
+                    R.id.sceneformFragment
+                ) as ArFragment
+
+            arLayout.setOnClickListener {
+                addObject(Uri.parse("pikachu.sfb"))
+            }
             viewModel.getLivePoints().observe(it.getLifecycleOwner(), Observer { points ->
                 arMapView.setMarker(points)
             })
@@ -92,12 +111,12 @@ class ArView @JvmOverloads constructor(
         }
     }
 
-    fun setArPoints(points: List<ArPointData>){
+    fun setArPoints(points: List<ArPointData>) {
         viewModel.setPoints(points)
     }
 
 
-    fun setNearestDistance(distance: Int){
+    fun setNearestDistance(distance: Int) {
         viewModel.setDistance(distance)
     }
 
@@ -112,6 +131,52 @@ class ArView @JvmOverloads constructor(
 //        arCameraView.onStart()
     }
 
+    // Arcore
+    private fun addObject(parse: Uri) {
+        val frame = arFragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(arFragment, hit.createAnchor(), parse)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun placeObject(fragment: ArFragment, createAnchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
+            .build()
+            .thenAccept {
+                addNodeToScene(fragment, createAnchor, it)
+            }
+            .exceptionally {
+                val builder = AlertDialog.Builder(dependencyProvider!!.getContext())
+                builder.setMessage(it.message)
+                    .setTitle("error!")
+                val dialog = builder.create()
+                dialog.show()
+                return@exceptionally null
+            }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(createAnchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+    private fun getScreenCenter(): Point {
+        val vw = findViewById<View>(R.id.arLayout)
+        return Point(vw.width / 2, vw.height / 2)
+    }
 
 
 }
